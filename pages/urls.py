76ac944cf6932ff7df5991bef1b2cf7f7aa3e676
SantiagoArias229 <s.arias.h2209@gmail.com>
2024-02-16 09:44:43 -0500

    Tutorial y actividades finalizado

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import HomePageView,AboutPageView,ProductIndexView,ProductShowView,ProductCreateView
+from .views import HomePageView,AboutPageView,ProductIndexView,ProductShowView,ProductCreateView,ContactsPageView,MessagePageView
 	
 urlpatterns = [
 	    path('', HomePageView.as_view(), name="home"),
         path('about/', AboutPageView.as_view(), name='about'),
+        path('contacts/', ContactsPageView.as_view(), name='contacts'),
         path('products/', ProductIndexView.as_view(), name='index'),
         path('products/create', ProductCreateView.as_view(), name='form'),
         path('products/<str:id>', ProductShowView.as_view(), name='show'),
+        path('message/', MessagePageView.as_view(), name='message'),
 
 
 	]
--- pages/views.py
+++ pages/views.py
@@ -2,11 +2,24 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views import View
 from django import forms
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,HttpResponseRedirect
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "home.html"
+class ContactsPageView(TemplateView):
+    template_name = 'pages/contacts.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact us",
+            "subtitle": "email:djangostore@gmail.com",
+            "description": "Ubi: Carrera 84#20a-8",
+            "author": "Phone:3003004567",
+        })
+
+        return context
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
 
@@ -23,10 +36,10 @@ class AboutPageView(TemplateView):
 
 class Product:
     products = [
-        {"id":"1", "name":"TV", "description":"Best TV"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+        {"id":"1", "name":"TV", "description":"Best TV","price":int("50")},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone","price":int("2000")},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast","price":int("3000")},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses","price":int("4000")}
     ]
 
 class ProductIndexView(View):
@@ -45,17 +58,22 @@ class ProductShowView(View):
 
 
     def get(self, request, id):
+        try:
+            product = Product.products[int(id) - 1]  
+        except (IndexError, ValueError): 
+            return HttpResponseRedirect('..') 
         viewData = {}
         product = Product.products[int(id)-1]
         viewData["title"] = product["name"] + " - Online Store"
         viewData["subtitle"] =  product["name"] + " - Product information"
         viewData["product"] = product
+        
 
         return render(request, self.template_name, viewData)
 
 class ProductForm(forms.Form):
     name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
+    price = forms.DecimalField(required=True, min_value=0.0)
 
 class ProductCreateView(View):
     template_name = 'products/create.html'
@@ -77,5 +95,7 @@ class ProductCreateView(View):
             viewData["title"] = "Create product"
             viewData["form"] = form
             return render(request, self.template_name, viewData)
+class MessagePageView(TemplateView):
+    template_name = "message.html"
 
 
