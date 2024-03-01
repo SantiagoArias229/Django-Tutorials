8a3051f9b9e9a524b664f7fdfae74eaf845ab893
SantiagoArias229 <s.arias.h2209@gmail.com>
2024-02-29 21:10:04 -0500

    Tutorial 3 Parte A y B listas

--- HELLOWORLD/settings.py
+++ HELLOWORLD/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,8 +122,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'pages/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+IMAGE_STORAGE_CLASS = 'pages.utils.ImageLocalStorage'
--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomePageView,AboutPageView,ProductIndexView,ProductShowView,ProductCreateView,ContactsPageView,MessagePageView
+from .views import *
+from pages.utils import ImageLocalStorage
 	
 urlpatterns = [
 	    path('', HomePageView.as_view(), name="home"),
@@ -9,6 +10,13 @@ urlpatterns = [
         path('products/create', ProductCreateView.as_view(), name='form'),
         path('products/<str:id>', ProductShowView.as_view(), name='show'),
         path('monda', MessagePageView.as_view(), name='message'),
+        path('cart/', CartView.as_view(), name='cart_index'),
+        path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+        path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+        path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+        path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save'),
+        path('image-not-di/', ImageViewNoDI.as_view(), name='imagenodi_index'),
+        path('image/save', ImageViewNoDI.as_view(), name='imagenotdi_save'), 
 
 
 	]
